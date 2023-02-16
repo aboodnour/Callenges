c4fbc45a385c1b73e76624ee4a2b1526b203b63c
aboodnour <aan505@gmail.com>
2023-02-16 17:18:09 +0200

    Updating XSS Challenges index

--- XSS/index.php
+++ XSS/index.php
@@ -6,6 +6,7 @@
     <h3> XSS Challenges</h3>
     <ul>
       <li> <a href="./BB_2302.php">Feb 2023 - XSS - Filtered Payload inside script tag - Real-life Bug Bounty Example</a></li>
+      <li> <a href="./BB_2302_v2.php">Feb 2023 - XSS - Filtered Payload inside script tag - v2 Slightly Harder Real-life Bug Bounty Example</a></li>
     </ul>
   </body>
 </html>
