2c986b7062da0008ebc404fbc182a55354f2c521
aboodnour <aan505@gmail.com>
2023-02-16 05:22:41 +0200

    Create XSS challenges index file

---
+++ XSS/index.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>Challenges - XSS</title>
+  </head>
+  <body>
+    <h3> XSS Challenges</h3>
+    <ul>
+      <li> <a href="./BB_2302.php">Feb 2023 - XSS - Filtered Payload inside script tag - Real-life Bug Bounty Example</a></li>
+    </ul>
+  </body>
+</html>
